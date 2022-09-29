076d71d5da34fd59de6f48797e7ed03d20824a98
AndypSheridan <andy.p.sheridan@gmail.com>
2022-09-29 10:27:43 +0000

    change jump touch to canvas only

--- assets/js/script.js
+++ assets/js/script.js
@@ -340,7 +340,7 @@ addEventListener("keydown", e => {
 });
 
 //Touch to jump event listener
-addEventListener("touchstart", e => {
+canvas.addEventListener("touchstart", e => {
     if(e.type === "touchstart"){
         e.preventDefault();
         if(!player.shouldJump){
